ba7b888df21cbfac5212809f0970085c2544ff54
HaleyB <haleyb128@gmail.com>
2018-09-17 14:54:56 -0500

    ready to deploy

--- portfolio-app/src/pages/noMatch/noMatch.js
+++ portfolio-app/src/pages/noMatch/noMatch.js
@@ -5,7 +5,7 @@ export class NoMatch extends Component {
     render(){
         return (
             <div>
-                <img src="https://i.imgur.com/nAk1bMi.jpg" className="error-image" id="bob-ross" alt="Darnit" />
+                <img src="https://i.imgur.com/wVIm7l8.jpg" className="error-image" id="error-message" alt="Darnit" />
             </div>
         );
     }
